db69b20ffd9580ca71e3e962c5509c4c5790ffd9
Jędrzej Romankiewicz <romankiewicz.j@gmail.com>
2023-04-17 09:27:31 +0200

    initial

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+type Payload struct {
+	Coords     []string `json:"coords"`
+	Desc       string   `json:"desc"`
+	Nick       string   `json:"nick"`
+	TargetNick string   `json:"target_nick"`
+	Wpbot      bool     `json:"wpbot"`
+}
+
+func main() {
+	p, err := json.Marshal(Payload{
+		Coords:     nil,
+		Desc:       "",
+		Nick:       "",
+		TargetNick: "",
+		Wpbot:      true})
+	bodyReader := bytes.NewReader(p)
+	if err != nil {
+		log.Println("failed to marshal payload", err)
+	}
+	client := http.Client{}
+	_, err = client.Post("https://go-pjatk-server.fly.dev/api/game", "application/json", bodyReader)
+	if err != nil {
+		log.Println("failed to post", err)
+	}
 }
